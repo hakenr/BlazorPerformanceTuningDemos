4c226cbfada95c5059dc79eab23ab88da8cffde5
Robert Haken <haken@havit.cz>
2024-03-03 17:55:36 +0100

    Add project files.

---
+++ BlazorPerformanceTuningDemos/BlazorPerformanceTuningDemos.Client/MeasuredComponentBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorPerformanceTuningDemos.Client
+{
+	public abstract class MeasuredComponentBase : ComponentBase
+	{
+		private Stopwatch _stopwatch;
+
+		protected MeasuredComponentBase()
+		{
+			_stopwatch = new Stopwatch();
+			_stopwatch.Start();
+
+			DumpMeasurementPoint("ctor");
+		}
+
+		protected override void OnInitialized()
+		{
+			DumpMeasurementPoint();
+		}
+
+		protected override void OnParametersSet()
+		{
+			DumpMeasurementPoint();
+		}
+
+		protected override Task OnAfterRenderAsync(bool firstRender)
+		{
+			DumpMeasurementPoint(measurementPointNameSuffix: $"(firstRender: {firstRender})");
+
+			return base.OnAfterRenderAsync(firstRender);
+		}
+
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			DumpMeasurementPoint();
+
+			base.BuildRenderTree(builder);
+		}
+
+		protected void DumpMeasurementPoint([CallerMemberName] string measurementPointName = null, string measurementPointNameSuffix = null)
+		{
+			Console.WriteLine($"[Measurement:{this.GetType().Name}.{measurementPointName}{measurementPointNameSuffix}] {_stopwatch.ElapsedMilliseconds}ms elapsed from previous measurement.");
+			_stopwatch.Restart();
+		}
+	}
+}
